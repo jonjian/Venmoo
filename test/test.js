3dde2bdd6e4ea9aa9088a85ca9169f6a0f7f71d8
Jake Luton <jake.luton92@gmail.com>
2018-01-09 12:09:37 -0800

    Added mocha and chai libraries, and test script

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var server = require('./..server/index.js');
+var server = require('./../server/index.js');
 
 describe('server', function() {
   it('should have an express server', function() {
