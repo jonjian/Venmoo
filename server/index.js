f130069958b5e5de4385892ae38ad2c3c8edaac0
arasdean <arasdean@gmail.com>
2018-01-09 16:25:04 -0800

    Provides or option for PORT

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
