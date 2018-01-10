01de10ed3d2f334f0a5281b1cfed0863bc304bf1
Jake Luton <jake.luton92@gmail.com>
2018-01-09 21:02:57 -0800

    Created database connection and getTransactionHistory database function. Turned off lint rule demanding that anonymous functions be named.

---
+++ database/index.js
@@ -0,0 +1,24 @@
+const { Client } = require('pg');
+
+const client = new Client();
+
+client.connect();
+
+const getTransactionHistory = function (userName) {
+  const queryString = `SELECT transactions.amount, transactions.status, transactions.type, 
+  transactions.created_timestamp, transactions.resolved_timestamp, transactions.description
+  FROM transactions, users
+  WHERE (transactions.sender_id=users.id AND users.name='${userName}')
+  OR (transactions.receiver_id=users.id AND users.name='${userName}')`
+
+  client.query(queryString)
+    .then((results) => {
+      console.log(results);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports.getTransactionHistory = getTransactionHistory;
+
