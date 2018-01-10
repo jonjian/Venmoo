d2be242a0fdb1078e2517c33a6982d4a7456cbc8
arasdean <arasdean@gmail.com>
2018-01-09 19:48:27 -0800

    Includes the posgres db connection and querying features

--- database/index.js
+++ database/index.js
@@ -1,4 +1,15 @@
-const {users, transactions} = require('./dummy-data.js')
+const pg = require('pg');
+// const { users, transactions } = require('./dummy-data.js')
 
-console.log(users[0]);
-console.log(transactions[0]);
+
+app.get('/db', (request, response) => {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', (err, result) => {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
