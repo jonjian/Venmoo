9e1cf21f737367a267ae5e73300190f74a5cf8bc
Jake Luton <jake.luton92@gmail.com>
2018-01-09 16:35:34 -0800

    Changed balance and amount data type from integer to money. Drops transactions before users because transactions depends on users.

--- database/schema.sql
+++ database/schema.sql
@@ -4,21 +4,21 @@ CREATE DATABASE venmoo;
 
 \connect venmoo;
 
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
-  balance INT NOT NULL
+  balance MONEY NOT NULL
 );
 
 CREATE TABLE transactions (
   id SERIAL NOT NULL PRIMARY KEY,
   sender_id INT references users(id),
   receiver_id INT references users(id),
-  amount INT NOT NULL,
-  status CHAR(8) NOT NULL,
+  amount MONEY NOT NULL,
+  status CHAR(12) NOT NULL,
   created_timestamp timestamp NOT NULL,
   resolved_timestamp timestamp,
   description VARCHAR(200)
