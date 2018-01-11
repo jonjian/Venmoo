01cf49588c00353e87061cba0fa098cd95741ed4
arasdean <arasdean@gmail.com>
2018-01-11 15:04:17 -0800

    Profilepage

--- client/src/components/ProfilePage.jsx
+++ client/src/components/ProfilePage.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Fixed profilepage merge conflict
 
 import Form from './Form.jsx'
 import TransactionHistory from './TransactionHistory.jsx';
 
 
 =======
+<<<<<<< HEAD
 =======
 import Form from './Form.jsx'
 >>>>>>> Adds css stylesheet and adds Form component
+=======
+>>>>>>> Fixed profilepage merge conflict
 import TransactionHistory from './TransactionHistory.jsx';
 >>>>>>> Just cleared up previous conflicts
 class ProfilePage extends React.Component {
@@ -23,13 +29,19 @@ class ProfilePage extends React.Component {
         PROFILE PAGE
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Fixed profilepage merge conflict
         <Form />
         <TransactionHistory transactionHist={this.props.transactionHist}/>
 
 =======
+<<<<<<< HEAD
 =======
         <Form />
 >>>>>>> Adds css stylesheet and adds Form component
+=======
+>>>>>>> Fixed profilepage merge conflict
         <TransactionHistory transactionHist={this.props.transactionHist}/>
 >>>>>>> Just cleared up previous conflicts
       </div>
