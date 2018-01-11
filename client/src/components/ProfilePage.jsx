9d638b0cbb62b81a8d8178fdac92b4f08861a3b9
arasdean <arasdean@gmail.com>
2018-01-11 15:03:17 -0800

    index and prof page

--- client/src/components/ProfilePage.jsx
+++ client/src/components/ProfilePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 import Form from './Form.jsx'
 import TransactionHistory from './TransactionHistory.jsx';
 
 
 =======
+=======
+import Form from './Form.jsx'
+>>>>>>> Adds css stylesheet and adds Form component
 import TransactionHistory from './TransactionHistory.jsx';
 >>>>>>> Just cleared up previous conflicts
 class ProfilePage extends React.Component {
@@ -18,10 +22,14 @@ class ProfilePage extends React.Component {
       <div>
         PROFILE PAGE
 <<<<<<< HEAD
+<<<<<<< HEAD
         <Form />
         <TransactionHistory transactionHist={this.props.transactionHist}/>
 
 =======
+=======
+        <Form />
+>>>>>>> Adds css stylesheet and adds Form component
         <TransactionHistory transactionHist={this.props.transactionHist}/>
 >>>>>>> Just cleared up previous conflicts
       </div>
