0882c2090c4a0de8912a2cf67e499e1a687279eb
arasdean <arasdean@gmail.com>
2018-01-11 15:02:13 -0800

    prof page

--- client/src/components/ProfilePage.jsx
+++ client/src/components/ProfilePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+<<<<<<< HEAD
 
 import Form from './Form.jsx'
 import TransactionHistory from './TransactionHistory.jsx';
 
 
+=======
+import TransactionHistory from './TransactionHistory.jsx';
+>>>>>>> Just cleared up previous conflicts
 class ProfilePage extends React.Component {
   constructor(props) {
     super(props);
@@ -13,9 +17,13 @@ class ProfilePage extends React.Component {
     return (
       <div>
         PROFILE PAGE
+<<<<<<< HEAD
         <Form />
         <TransactionHistory transactionHist={this.props.transactionHist}/>
 
+=======
+        <TransactionHistory transactionHist={this.props.transactionHist}/>
+>>>>>>> Just cleared up previous conflicts
       </div>
     );
   }
