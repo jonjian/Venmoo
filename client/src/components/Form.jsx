d42984ebe8dfbe84a9442d422b69d38eac5972a3
arasdean <arasdean@gmail.com>
2018-01-10 20:58:02 -0800

    Adding handlers

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -7,6 +7,24 @@ class Form extends React.Component {
     super(props);
 
     this.state = { isPayment: true };
+
+    this.togglePaymentTrue = this.togglePaymentTrue.bind(this);
+    this.togglePaymentFalse = this.togglePaymentFalse.bind(this);
+  }
+
+
+  togglePaymentTrue(event) {
+    event.preventDefault();
+    if(!this.state.isPayment) {
+      this.setState({ isPayment: true });
+    }
+  }
+
+  togglePaymentFalse(event) {
+    event.preventDefault();
+    if(this.state.isPayment === true) {
+      this.setState({ isPayment: false });
+    }
   }
 
   render() {
@@ -14,21 +32,25 @@ class Form extends React.Component {
       <div id='form'>
         FORM STUFF
         <br />
-        <button type='submit'> Pay </button>
-        <button type='submit'> Request </button>
-        <br />
-        <br />
-        <label> To: </label>
-        <input type='textarea' />
-        <br />
-        <br />
-        <label> Amount: </label>
-        <input type='textarea' />
-        <br />
-        <br />
-        <label> Message: </label>
-        <input type='textarea' />
-
+        <form>
+          <h1> {this.state.isPayment ? 'TRUE' : 'FALSE'} </h1>
+          <div className='tab'>
+            <button type="submit" onClick={this.togglePaymentTrue}> Pay </button>
+            <button type="submit" onClick={this.togglePaymentFalse}> Request </button>
+          </div>
+          <br />
+          <br />
+          <label> To: </label>
+          <input type="textarea" />
+          <br />
+          <br />
+          <label> Amount: </label>
+          <input type="textarea" />
+          <br />
+          <br />
+          <label> Message: </label>
+          <input type="textarea" />
+        </form>
       </div>
   )}
 }
