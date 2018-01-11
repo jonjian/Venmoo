b9951d9ea1a71a1fd18d62c100b046c7e7b52721
arasdean <arasdean@gmail.com>
2018-01-10 20:59:55 -0800

    Bundle js again

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -6,55 +6,89 @@ class Form extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { isPayment: true };
+    this.state = {
+      isPayment: true,
+      otherUser: '',
+      amount: '',
+      message: ''
+    };
 
     this.togglePaymentTrue = this.togglePaymentTrue.bind(this);
     this.togglePaymentFalse = this.togglePaymentFalse.bind(this);
+    this.otherUserChangeHandler = this.otherUserChangeHandler.bind(this);
+    this.amountChangeHandler = this.amountChangeHandler.bind(this);
+    this.messageChangeHandler = this.messageChangeHandler.bind(this);
+    this.formSubmitHandler = this.formSubmitHandler.bind(this);
   }
 
 
   togglePaymentTrue(event) {
     event.preventDefault();
-    if(!this.state.isPayment) {
+    if (!this.state.isPayment) {
       this.setState({ isPayment: true });
     }
   }
 
   togglePaymentFalse(event) {
     event.preventDefault();
-    if(this.state.isPayment === true) {
+    if (this.state.isPayment === true) {
       this.setState({ isPayment: false });
+    }
+  }
+
+  otherUserChangeHandler(event) {
+    event.preventDefault();
+    this.setState({otherUser: event.target.value})
+  }
+
+  amountChangeHandler(event) {
+    event.preventDefault();
+    this.setState({amount: event.target.value})
+  }
+
+  messageChangeHandler(event) {
+    event.preventDefault();
+    this.setState({message: event.target.value})
+  }
+
+  formSubmitHandler(event) {
+    event.preventDefault();
+    if(this.state.isPayment) {
+      console.log('Sending ' + this.state.otherUser + ' $' + this.state.amount + ' saying ' + this.state.message);
+    } else {
+      console.log('Requesting ' + this.state.otherUser + ' $' + this.state.amount + ' saying ' + this.state.message);
     }
   }
 
   render() {
     return(
       <div id='form'>
-        FORM STUFF
         <br />
-        <form>
-          <h1> {this.state.isPayment ? 'TRUE' : 'FALSE'} </h1>
-          <div className='tab'>
-            <button type="submit" onClick={this.togglePaymentTrue}> Pay </button>
-            <button type="submit" onClick={this.togglePaymentFalse}> Request </button>
-          </div>
+        <h1> {this.state.isPayment ? 'TRUE' : 'FALSE'} </h1>
+        <div className='tab'>
+          <button type="submit" onClick={this.togglePaymentTrue}> Pay </button>
+          <button type="submit" onClick={this.togglePaymentFalse}> Request </button>
+        </div>
+
+        <form onSubmit={this.formSubmitHandler}>
           <br />
           <br />
           <label> To: </label>
-          <input type="textarea" />
+          <input type="textarea" onChange={this.otherUserChangeHandler} />
           <br />
           <br />
           <label> Amount: </label>
-          <input type="textarea" label="$" />
+          $<input type="textarea" onChange={this.amountChangeHandler} />
           <br />
           <br />
           <label> Message: </label>
-          <input type="textarea" />
+          <input type="textarea" onChange={this.messageChangeHandler} />
           <br />
           <br />
           <br />
           {this.state.isPayment ?
-          (<div> <button id="decisionBtn" type="submit"> Pay </button> </div>)
+          (<div> <button id="decisionBtn" type="submit" value="Submit">
+            Pay </button> </div>)
           :
           (<div> <button id="decisionBtn" type="submit"> Request </button> </div>)}
         </form>
