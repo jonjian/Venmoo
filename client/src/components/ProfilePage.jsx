e4fe8c1bfea8ff43ee84129d6df223064bd295c8
arasdean <arasdean@gmail.com>
2018-01-11 10:31:22 -0800

    Updates ProfilePage.jsx

--- client/src/components/ProfilePage.jsx
+++ client/src/components/ProfilePage.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import TransactionHistory from './TransactionHistory.jsx';
 
-=======
 import Form from './Form.jsx'
->>>>>>> Creates the form component with fields to, amount, and message
-=======
 import TransactionHistory from './TransactionHistory.jsx';
 
->>>>>>> Something is happening
+
 class ProfilePage extends React.Component {
   constructor(props) {
     super(props);
@@ -21,18 +15,10 @@ class ProfilePage extends React.Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
         PROFILE PAGE
+        <Form />
         <TransactionHistory transactionHist={this.props.transactionHist}/>
-=======
-      PROFILE PAGE
-      <Form />
->>>>>>> Creates the form component with fields to, amount, and message
-=======
-        PROFILE PAGE
-        <TransactionHistory transactionHist={this.props.transactionHist}/>
->>>>>>> Something is happening
+
       </div>
     );
   }
