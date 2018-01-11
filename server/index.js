e905f538262e6d2d796d5e5b5c5f5719fffe914d
arasdean <arasdean@gmail.com>
2018-01-10 21:42:58 -0800

    server index js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+<<<<<<< HEAD
 app.get('/user/:id', (req, res) => {
   const { id } = req.params;
   if (isNaN(Number(id)) || Number(id) % 1 !== 0) {
@@ -20,6 +21,32 @@ app.get('/user/:id', (req, res) => {
   }
 });
 
+=======
+// app.get('/db', (request, response) => {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', (err, result) => {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else {
+//         response.statusCode = 200;
+//         response.send(JSON.stringify({results: result.rows}));
+//        };
+//     });
+//   });
+// });
+
+app.get('/db', (req, res) => {
+  res.send('HELLO WORLD!!!')
+});
+
+
+
+// app.get('/', (request, response) => {
+//   res.send({'yo'})
+// });
+
+>>>>>>> Testing get route for db
 if (!module.parent) {
   app.listen(PORT);
   console.log(`Listening on ${PORT}`);
