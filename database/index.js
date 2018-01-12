2ce03d5e590b5e241c850ce76c3d8feb64cbeaa4
alex <alexwein@gmail.com>
2018-01-12 12:06:19 -0800

    added real url to yaml, reverted to unencoded

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const client = new Client({
-  connectionString: `${decodeURIComponent(process.env.DATABASE_URL)}?ssl=true`,
+  connectionString: `${process.env.DATABASE_URL}?ssl=true`,
   ssl: true,
 });
 
