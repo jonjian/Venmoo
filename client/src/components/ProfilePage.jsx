5e2ad576a836d3549d0fad64266a2cfed04e347f
arasdean <arasdean@gmail.com>
2018-01-12 10:46:23 -0800

    Stylistic changes to profpage and transactionhist components

--- client/src/components/ProfilePage.jsx
+++ client/src/components/ProfilePage.jsx
@@ -24,6 +24,7 @@ class ProfilePage extends React.Component {
               <h3>{`${this.props.user.balance}`}</h3>
             </div>
         </div>
+        <div> Transaction History </div> 
         <div className="scrollingTransactions">
         <TransactionHistory transactionHist={this.props.transactionHist} user={this.props.user}/>
         </div>
