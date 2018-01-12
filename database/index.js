0330504b253943479d6bac8540deb66c1151dad9
alex <alexwein@gmail.com>
2018-01-11 21:24:13 -0800

    encoding .env

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const client = new Client({
-  connectionString: `${process.env.DATABASE_URL}?ssl=true`,
+  connectionString: `${decodeURIComponent(process.env.DATABASE_URL)}?ssl=true`,
   ssl: true,
 });
 
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const db = require('../database');
 const expressRenderJsx = require('express-render-jsx');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
