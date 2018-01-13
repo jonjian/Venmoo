4b0e39cbc90152bd4c448efabb0452c5fc256d37
arasdean <arasdean@gmail.com>
2018-01-12 21:11:11 -0800

    Fixing tests

--- server/index.js
+++ server/index.js
@@ -26,11 +26,11 @@ app.post('/payment', (req, res) => {
       const { id } = data.rows[0];
       db.createTransaction(senderObj.id, id, amount, isPayment)
         .then(db.updateBalances)
-        .then(() => {
-          res.statusCode = 201;
-          res.end();
-        })
         .catch(() => console.error())
+    })
+    .then(() => {
+      res.statusCode = 201;
+      res.end();
     })
     .catch(() => { console.error(); });
 });
