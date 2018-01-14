cec8163e5a5fe86197446fafdf7638f0c0a0af9d
jluton <32603261+jluton@users.noreply.github.com>
2018-01-14 11:53:25 -0800

    Even more cleanup

--- server/index.js
+++ server/index.js
@@ -98,29 +98,6 @@ app.get('/', reactRoute);
 
 app.get('/signup', reactRoute);
 
-
-// });
-
-// app.get('/profilepage/username/:name', (req, res) => {
-//   const { name } = req.params;
-
-//   const responseData = {};
-
-//   db.getUserByName(name)
-//     .then((userData) => {
-//       checkDatabaseResponse(userData, res);
-//       responseData.user = userData.rows[0];
-//       db.getTransactionHistory(name)
-//         .then((transactionData) => {
-//           checkDatabaseResponse(transactionData, res);
-//           responseData.transactions = transactionData.rows;
-//           res.redirect('/profilepage').json(responseData);
-//         })
-//         .catch(err => console.error(err));
-//     })
-//     .catch(err => console.error(err));
-// });
-
 app.post('/transaction/accept/:id-:status', (req, res) => {
   const { id, status } = req.params;
   if (isNaN(Number(id)) || Number(id) % 1 !== 0) {
